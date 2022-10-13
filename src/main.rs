5deaaa90ca08b51f36498502da16ed4b52777f6c
Romain Cahuet <romain.cahuet@rentacloud.fr>
2022-10-13 09:26:48 +0200

    [RUST][ADD] added a basic add funtion and a test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+    println!("{}", my_add(2,3));
+}
+
+fn my_add(a: i64, b: i64) -> i64 {
+    a+b
+}
+
+#[cfg(test)]
+mod tests {
+    // importing names from outer
+    use super::*;
+
+    #[test]
+    fn test_add() {
+	assert_eq!(my_add(2, 3), 5);
+    }
 }
